34cc130a95e8f7bf09d866667767c8ac02392b91
Pavlos Elpidorou <pavlos@prudentsigma.com>
2026-03-22 12:24:44 +0200

    Fix streamlit-authenticator compatibility - remove deprecated preauthorized parameter

--- dashboard.py
+++ dashboard.py
@@ -26,8 +26,7 @@ try:
         config['credentials'],
         config['cookie']['name'],
         config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
+        config['cookie']['expiry_days']
     )
     
     # Run authentication
